d5fc59e36ba7f1a941f9f0e59935d33c2f5b95c4
Anuar Abdullahi <universe.city.trd@gmail.com>
2020-09-05 23:41:56 +0200

    Created a Google sign-up button. Applied the Mui SVGIcon component.

--- src/components/organisms/sign-up/sign-up.styles.jsx
+++ src/components/organisms/sign-up/sign-up.styles.jsx
@@ -1,4 +1,13 @@
+import React from 'react';
 import styled from 'styled-components';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+// Icons
+import { ReactComponent as GoogleLogo } from '../../../assets/svg/google-logo.svg';
+
+export const GoogleIcon = props => {
+	return <SvgIcon {...props} component={GoogleLogo} viewBox='0 0 512 512' />;
+};
 
 export const Box = styled.div`
 	display: flex;
@@ -11,4 +20,10 @@ export const Box = styled.div`
 	& .button {
 		margin-top: ${props => props.theme.spacing(5)};
 	}
+`;
+
+export const Text = styled.div`
+	font-size: ${props => props.theme.fontSize(props.fontSize)};
+	color: ${props => props.fontColor};
+	text-align: center;
 `;
--- src/themes/styled-components.theme.js
+++ src/themes/styled-components.theme.js
@@ -1,6 +1,7 @@
 const theme = {
 	radius: number => `${number * 4}px`,
 	spacing: number => `${number * 4}px`,
+	fontSize: number => `${number * 4}px`,
 };
 
 export default theme;
