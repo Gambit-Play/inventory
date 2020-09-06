c8e22771131818b4b5de28a593b5fbb1bd5542f3
Anuar Abdullahi <universe.city.trd@gmail.com>
2020-09-06 14:40:44 +0200

    Created a login component for the login-screen.

--- src/components/organisms/sign-in/sign-in.component.jsx
+++ src/components/organisms/sign-in/sign-in.component.jsx
@@ -9,25 +9,20 @@ import * as ROUTES from '../../../routes/routes';
 
 // Redux
 import {
-	selectDisplayName,
 	selectEmail,
 	selectPassword,
-	selectConfirmPassword,
 	selectCurrentUser,
-	selectErrorConfirmPassword,
-	selectErrorDisplayName,
 	selectErrorEmail,
 	selectErrorPassword,
 } from '../../../redux/users/users.selectors';
 import {
 	setUserCredential,
-	signUpStart,
 	clearUserCredentials,
 	clearInputErrors,
 } from '../../../redux/users/users.actions';
 
 // Components
-import { Box, GoogleIcon, Text } from './sign-up.styles';
+import { Box, GoogleIcon, Text } from '../sign-up/sign-up.styles';
 
 // Icons
 // import { ReactComponent as GoogleIcon } from '../../../assets/svg/google-logo.svg';
@@ -36,16 +31,11 @@ import { Box, GoogleIcon, Text } from './sign-up.styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-const SignUp = ({
-	selectDisplayName,
+const SignIn = ({
 	selectEmail,
 	selectPassword,
-	selectConfirmPassword,
 	setUserCredential,
 	selectCurrentUser,
-	signUpStart,
-	errorConfirmPassword,
-	errorDisplayName,
 	errorEmail,
 	errorPassword,
 	clearUserCredentials,
@@ -73,17 +63,6 @@ const SignUp = ({
 		<form>
 			<Box>
 				<TextField
-					id='displayName'
-					name='displayName'
-					label='Display Name'
-					value={selectDisplayName}
-					type='text'
-					onChange={handleChange}
-					className='input'
-					helperText={errorDisplayName}
-					error={errorDisplayName ? true : false}
-				/>
-				<TextField
 					id='email'
 					name='email'
 					label='Email'
@@ -105,28 +84,17 @@ const SignUp = ({
 					helperText={errorPassword}
 					error={errorPassword ? true : false}
 				/>
-				<TextField
-					id='confirmPassword'
-					name='confirmPassword'
-					label='Confirm Password'
-					value={selectConfirmPassword}
-					type='password'
-					onChange={handleChange}
-					className='input'
-					helperText={errorConfirmPassword}
-					error={errorConfirmPassword ? true : false}
-				/>
 				<Button
 					variant='contained'
 					color='primary'
 					size='small'
 					className='button'
-					onClick={signUpStart}
+					// onClick={signUpStart}
 				>
-					Sign Up
+					Login
 				</Button>
 				<Text fontColor='#47525dcc' fontSize={4} className='button'>
-					Or sign up with Google:
+					Or login with Google:
 				</Text>
 				<Button
 					variant='outlined'
@@ -136,23 +104,18 @@ const SignUp = ({
 					// onClick={handleSignUp}
 					startIcon={<GoogleIcon />}
 				>
-					Sign Up
+					Login
 				</Button>
 			</Box>
 		</form>
 	);
 };
 
-SignUp.propTypes = {
-	selectDisplayName: PropTypes.string.isRequired,
+SignIn.propTypes = {
 	selectEmail: PropTypes.string.isRequired,
 	selectPassword: PropTypes.string.isRequired,
-	selectConfirmPassword: PropTypes.string.isRequired,
 	setUserCredential: PropTypes.func.isRequired,
 	selectCurrentUser: PropTypes.any,
-	signUpStart: PropTypes.func.isRequired,
-	errorConfirmPassword: PropTypes.string.isRequired,
-	errorDisplayName: PropTypes.string.isRequired,
 	errorEmail: PropTypes.string.isRequired,
 	errorPassword: PropTypes.string.isRequired,
 	clearUserCredentials: PropTypes.func.isRequired,
@@ -160,13 +123,9 @@ SignUp.propTypes = {
 };
 
 const mapStateToProps = createStructuredSelector({
-	selectDisplayName: selectDisplayName,
 	selectEmail: selectEmail,
 	selectPassword: selectPassword,
-	selectConfirmPassword: selectConfirmPassword,
 	selectCurrentUser: selectCurrentUser,
-	errorConfirmPassword: selectErrorConfirmPassword,
-	errorDisplayName: selectErrorDisplayName,
 	errorEmail: selectErrorEmail,
 	errorPassword: selectErrorPassword,
 });
@@ -174,9 +133,8 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = dispatch => ({
 	setUserCredential: (inputName, value) =>
 		dispatch(setUserCredential(inputName, value)),
-	signUpStart: () => dispatch(signUpStart()),
 	clearUserCredentials: () => dispatch(clearUserCredentials()),
 	clearInputErrors: () => dispatch(clearInputErrors()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- src/pages/login-page/login-screen.component.jsx
+++ src/pages/login-page/login-screen.component.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+// Redux
+import { selectIsNewUser } from '../../redux/users/users.selectors';
 
 // Components
 import { LoginImage, Box } from './login-screen.styles';
 import SignUp from '../../components/organisms/sign-up/sign-up.component';
+import SignIn from '../../components/organisms/sign-in/sign-in.component';
 
-const LoginScreen = () => {
+const LoginScreen = ({ isNewUser }) => {
 	return (
 		<LoginImage>
-			<Box>
-				<SignUp />
-			</Box>
+			<Box>{isNewUser ? <SignUp /> : <SignIn />}</Box>
 		</LoginImage>
 	);
 };
 
-export default LoginScreen;
+LoginScreen.propTypes = {
+	isNewUser: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+	isNewUser: selectIsNewUser,
+});
+
+export default connect(mapStateToProps)(LoginScreen);
